fe3f0e877394973009ee709f2da489d13248ee59
Sandra Pastrana <spastrana@hotmail.com>
2019-06-26 20:37:56 -0500

    folder structure

---
+++ FifaApp/app.py
@@ -0,0 +1,19 @@
+import os
+ 
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+
+if __name__ == "__main__":
+    app.run()
