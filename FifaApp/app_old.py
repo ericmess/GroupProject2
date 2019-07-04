936a983d7ab09d98a34fff7d95e4d50710f7c84e
Sandra Pastrana <spastrana@hotmail.com>
2019-07-03 21:01:26 -0500

    final

--- FifaApp/app_old.py
+++ FifaApp/app_old.py
@@ -2,11 +2,8 @@ import os
  
 import pandas as pd
 import numpy as np
-import sqlite3 as sql
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-import sqlalchemy 
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -52,7 +49,6 @@ results = pd.read_sql("SELECT * from ResultsByCountry", conn) #import pandas
 def index():
     """Return the homepage."""
     return render_template("index.html")
-  
 
 
 @app.route("/years")
@@ -135,81 +131,8 @@ def Results(country):
 
        i+=1
    return jsonify(results_dict)
-#
-# Bimi's section
-#
-db.Model.metadata.reflect(db.engine)
-
-cnx = sql.connect('db/WorldCup.sqlite')
-df = pd.read_sql_query("SELECT * FROM ResultsByCountry2 order by wins desc, runners_up desc, country", cnx)
-df.groupby(['country','wins','runners_up']).size().unstack().plot(kind='bar',stacked=True)
-
-class Result(db.Model):
-    __tablename__ = 'ResultsByCountry2'
-    __table_args__ = { 'extend_existing': True }
-    id = db.Column(db.Integer, primary_key=True)
-
-@app.route("/save_chart")
-def save_chart():
-#Read in data & create total column
-  stacked_bar_data = df
-
-  #Set general plot properties
-  sns.set_style("white")
-  sns.set_context({"figure.figsize": (24, 10)})
-
-  #Plot 1 - background - "total" (top) series
-  sns.barplot(x = stacked_bar_data.country, y = stacked_bar_data.total_finals, color = "red")
-
-  #Plot 2 - overlay - "bottom" series
-  bottom_plot = sns.barplot(x = stacked_bar_data.country, y = stacked_bar_data.wins, color = "#0000A3")
-
-  topbar = plt.Rectangle((0,0),1,1,fc="red", edgecolor = 'none')
-  bottombar = plt.Rectangle((0,0),1,1,fc='#0000A3',  edgecolor = 'none')
-  l = plt.legend([bottombar, topbar], ['Wins', 'Runners Up'], loc=1, ncol = 2, prop={'size':16})
-  l.draw_frame(False)
-
-  #Optional code - Make plot look nicer
-  sns.despine(left=True)
-  bottom_plot.set_ylabel("Wins / Runners Up")
-  bottom_plot.set_xlabel("Countries")
-
-  #Set fonts to consistent 16pt size
-  for item in ([bottom_plot.xaxis.label, bottom_plot.yaxis.label] +
-              bottom_plot.get_xticklabels() + bottom_plot.get_yticklabels()):
-      item.set_fontsize(16)
-  
-  loc='static/Images/wins_runners_up.png'
-  plt.savefig(loc, bbox_inches='tight')
-  plt.close('all')
-  return loc
 
 
-@app.route("/wins")
-def wins():
-    wins = Result.query.all()
-    loc = save_chart()
-    return render_template("wins.html", winners=wins, chart=loc)
-
-# @app.route("/wins/<year>")
-# def wins(year):
-#   wins = Result.query.all()
-#   return render_template("wins.html", year=year, winners=wins, chart='/' + save_chart())
-
-@app.route("/win/<year>")
-def win(year):
-  with sql.connect("data/WorldCup.sqlite3") as con:
-    cur = con.cursor()
-    cur.execute("select * from ResultsByCountry2 where Country in ( select Country from YearCountry where Year is ? )", (year,) )
-    win = cur.fetchall()  
-    #return render_template("result.html", year=year, winner=result, chart='/static/img/wins_runners_up.png')
-    return render_template("win.html", year=year, winner=win, chart='/' + save_chart())
-
-#
-# End Bimi's section
-#
 
 if __name__ == "__main__":
     app.run()
-
-
---
+++ FifaApp/static/js/linechart.js
@@ -0,0 +1,97 @@
+
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+var parseTime = d3.timeParse("%Y")
+    bisectDate = d3.bisector(function(d) { return d.year; }).left;
+
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+var line = d3.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(d.value); });
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    console.log("test");
+
+d3.json("../static/js/NewLine.json", function(error, data) {
+    if (error) throw error;
+    console.log(data);
+
+    data.forEach(function(d) {
+      d.year = parseTime(d.year);
+      d.value = +d.value;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.year; }));
+    y.domain([d3.min(data, function(d) { return d.value; }) / 1.005, d3.max(data, function(d) { return d.value; }) * 1.005]);
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(6).tickFormat(function(d) { return parseInt(d / 1000) + "k"; }))
+      .append("text")
+        .attr("class", "axis-title")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .attr("fill", "#5D6971")
+        .text("Population)");
+
+    g.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+
+    var focus = g.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .attr("y2", height);
+
+    focus.append("line")
+        .attr("class", "y-hover-line hover-line")
+        .attr("x1", width)
+        .attr("x2", width);
+
+    focus.append("circle")
+        .attr("r", 7.5);
+
+    focus.append("text")
+        .attr("x", 15)
+      	.attr("dy", ".31em");
+
+    svg.append("rect")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemove);
+
+    function mousemove() {
+      var x0 = x.invert(d3.mouse(this)[0]),
+          i = bisectDate(data, x0, 1),
+          d0 = data[i - 1],
+          d1 = data[i],
+          d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+      focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
+      focus.select("text").text(function() { return d.value; });
+      focus.select(".x-hover-line").attr("y2", height - y(d.value));
+      focus.select(".y-hover-line").attr("x2", width + width);
+    }
+});
